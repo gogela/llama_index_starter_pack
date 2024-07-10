abd5d715662574b4fe443f3e6b0038d2743406cb
gogela <gogela@atlas.cz>
2024-07-10 13:56:35 +0200

    Update insertDocument.tsx - change localhost url

--- flask_react/react_frontend/src/apis/insertDocument.tsx
+++ flask_react/react_frontend/src/apis/insertDocument.tsx
@@ -2,8 +2,8 @@ const insertDocument = async (file: File) => {
   const formData = new FormData();
   formData.append('file', file);
   formData.append('filename_as_doc_id', 'true');
-
-  const response = await fetch('http://localhost:5601/uploadFile', {
+//  const response = await fetch('http://localhost:5601/uploadFile', {
+  const response = await fetch('/uploadFile', {
     mode: 'cors',
     method: 'POST',
     body: formData,
