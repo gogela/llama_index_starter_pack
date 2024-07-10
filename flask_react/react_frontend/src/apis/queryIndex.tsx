2206f8ce902766f29459a73b9916d5fe65318953
gogela <gogela@atlas.cz>
2024-07-10 14:53:11 +0200

    Update queryIndex.tsx

--- flask_react/react_frontend/src/apis/queryIndex.tsx
+++ flask_react/react_frontend/src/apis/queryIndex.tsx
@@ -13,7 +13,7 @@ export type QueryResponse = {
 
 const queryIndex = async (query: string): Promise<QueryResponse> => {
   // const queryURL = new URL('http://localhost:5601/query?');
-  const queryURL = new URL('/query?');
+  const queryURL = new URL('/query',window.location.href);
   queryURL.searchParams.append('text', query);
 
   const response = await fetch(queryURL, { mode: 'cors' });
